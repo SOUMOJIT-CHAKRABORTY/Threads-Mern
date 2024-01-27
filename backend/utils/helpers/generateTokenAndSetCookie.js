4a801da1c22705cdab676a178b68c4183e4740de
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-01-28 01:32:59 +0530

    :hammer: setting node env

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 
-	res.cookie("jwt", token, {
-		httpOnly: true, // more secure
-		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-		sameSite: "strict", // CSRF
-	});
+  res.cookie("jwt", token, {
+    httpOnly: true, // more secure
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // CSRF
+  });
 
-	return token;
+  return token;
 };
 
 export default generateTokenAndSetCookie;
