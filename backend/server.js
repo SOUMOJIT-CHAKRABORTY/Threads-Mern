b3849c44a01d015ef886ffe9ad09b096995f3f8a
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-01-28 01:06:26 +0530

    :hammer: small fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
-// import cors from "cors";
+import cors from "cors";
 
 dotenv.config();
 
@@ -25,7 +25,7 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
 // Routes
 app.use("/api/users", userRoutes);
